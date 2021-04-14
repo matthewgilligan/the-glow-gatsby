9ae3f278f89992c77d33735b29c3cbbe2f26b43b
Matthew Gilligan <mcgilligan97@gmail.com>
2021-04-14 20:44:05 +0100

    Refactor new music graphql

--- src/components/Albums/index.js
+++ src/components/Albums/index.js
@@ -4,87 +4,32 @@ import { graphql, useStaticQuery } from 'gatsby';
 import './styles.scss';
 import Album from './Album';
 
-const Albums = () => {
-    const data = useStaticQuery(graphql`
-        query {
-            pop: allStrapiAlbums ( filter: { genre:{ name: { eq: "Pop" } } } ) {
-                edges {
-                    node {
-                        title
-                        artists {
-                            englishName
-                        }
-                        genre {
-                            name
-                        }
-                        cover {
-                            url
-                        }
-                    }
-                }
-            }
-            rock: allStrapiAlbums ( filter: { genre:{ name: { eq: "Rock" } } } ) {
-                edges {
-                    node {
-                        title
-                        artists {
-                            englishName
-                        }
-                        genre {
-                            name
-                        }
-                        cover {
-                            url
-                        }
-                    }
-                }
-            }
-            hiphop: allStrapiAlbums ( filter: { genre:{ name: { eq: "Hip-Hop" } } } ) {
-                edges {
-                    node {
-                        title
-                        artists {
-                            englishName
-                        }
-                        genre {
-                            name
-                        }
-                        cover {
-                            url
-                        }
-                    }
-                }
-            }
-        }
-    `);
+const Albums = ({ edges }) => {
+  if (!Array.isArray(edges)) return null;
 
-    const albums = data.hiphop.edges;
-
-    if (!Array.isArray(albums)) return null;
-
-    if (albums.length < 1) {
-        return (
-            <div className="products">
-                <p>
-                    Content is on the way - stay tuned!
-                </p>
-            </div>
-        );
-    };
-
+  if (edges.length < 1) {
     return (
-        <div className="albums">
-            {albums.map((edge, pos) => {
-                const configAlbum = {
-                    ...edge.node
-                };
+      <div className="products">
+        <p>
+          Content is on the way - stay tuned!
+        </p>
+      </div>
+    );
+  };
 
-                return (
-                    <Album key={pos} {...configAlbum} />
-                )
-            })}
-        </div>
-    )
+  return (
+    <div className="albums">
+      {edges.map((edge, pos) => {
+        const configAlbum = {
+          ...edge.node
+        };
+
+        return (
+          <Album key={pos} {...configAlbum} />
+        )
+      })}
+    </div>
+  )
 };
 
 export default Albums;
--- src/pages/new-music.js
+++ src/pages/new-music.js
@@ -4,12 +4,67 @@ import './styles.scss';
 import IndexLayout from './../layouts/IndexLayout';
 import Albums from './../components/Albums';
 
-const NewMusicIndex = () => {
+const NewMusicIndex = ({ data }) => {
+  const { pop, rock, hiphop } = data;
+
 	return (
 		<IndexLayout title="New Music">
-			<Albums />
+			<Albums {...pop} />
 		</IndexLayout>  
 	);
 };
+
+export const query = graphql`
+  query {
+    pop: allStrapiAlbums ( filter: { genre:{ name: { eq: "Pop" } } } ) {
+      edges {
+        node {
+          title
+          artists {
+            englishName
+          }
+          genre {
+            name
+          }
+          cover {
+            url
+          }
+        }
+      }
+    }
+    rock: allStrapiAlbums ( filter: { genre:{ name: { eq: "Rock" } } } ) {
+      edges {
+        node {
+          title
+          artists {
+            englishName
+          }
+          genre {
+            name
+          }
+          cover {
+            url
+          }
+        }
+      }
+    }
+    hiphop: allStrapiAlbums ( filter: { genre:{ name: { eq: "Hip-Hop" } } } ) {
+      edges {
+        node {
+          title
+          artists {
+            englishName
+          }
+          genre {
+            name
+          }
+          cover {
+            url
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default NewMusicIndex;
