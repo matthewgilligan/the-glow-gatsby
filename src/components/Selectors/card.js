96475c5fd5d2db4e4d27a0eea298d8aa283ccb36
Matthew Gilligan <mcgilligan97@gmail.com>
2021-04-24 16:09:43 +0100

    Tidy up selector files

--- src/components/Selectors/card.js
+++ src/components/Selectors/card.js
@@ -1,27 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby';
-import Img from 'gatsby-image';
 
 const Selector = ({ node }) => {
-  console.log(node);
   const { artists } = node;
 
   return (
     <div className="playlist">
       <h1>{artists[0].englishName}</h1>
-      {/* <div className="imageWrapper">
-        <Link to={`./${slug}`}>
-          <Img
-            fluid={coverImage.fluid}
-            key={coverImage.fluid.src}
-            alt={coverImage.title}
-            className="image">
-          </Img>
-        </Link>
-      </div>
-      <div className="details">
-        <h2 className="artist">{artist[0].englishName}</h2>
-      </div> */}
     </div>
   )
 };
--- src/components/Selectors/index.js
+++ src/components/Selectors/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 
 import './styles.scss';
 import Selector from './card.js';
