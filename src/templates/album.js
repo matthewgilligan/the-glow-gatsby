6213ed4b72c2674490e5000edfb6ba9dcbaf99d6
Matthew Gilligan <mcgilligan97@gmail.com>
2021-05-20 20:12:38 +0100

    Build banner for album page

--- src/templates/album.js
+++ src/templates/album.js
@@ -18,10 +18,52 @@ const NavBarImg = styled.div`
   }
 `;
 
-const Container = styled.div`
-  width: calc(100vw - 80px);
-  float: right;
+const Banner = styled.div`
+  position: relative;
+  width: 100%;
+  height: 350px;
+  margin-bottom: 150px;
+  background-color: pink;
+`;
+
+const BannerWrap = styled.div`
+  position: absolute;
+  display: flex;
+  max-width: 1350px;
+  width: 100%;
   padding: 0 60px;
+  justify-content: space-between;
+  bottom: 0;
+  transform: translateY(150px);
+`;
+
+const TextWrap = styled.div`
+  max-width: 700px;
+  width: 60%;
+  h1 {
+    margin: 0;
+  }
+  h2 {
+    font-size: 18px;
+    margin: 0 0 10px;
+  }
+`;
+
+const ImgWrap = styled.div`
+  width: 400px;
+  height: 400px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const Content = styled.div`
+  width: 60%;
+  max-width: 700px;
+  padding: 0 60px;
+  transform: translateY(-100px);
 `;
 
 const AlbumTemplate = ({ data }) => {
@@ -34,21 +76,29 @@ const AlbumTemplate = ({ data }) => {
       <NavBarImg>
         <img src={`${process.env.IMAGE_BASE_URL}${cover[0].url}`} alt=""/>
       </NavBarImg>
-      <Container>
-        <h1>{englishTitle}</h1>
-        {japaneseTitle && <h2>{japaneseTitle}</h2>}
-        <Link to={`/artist/${artists[0].slug}`}>
-          <h2>{artists[0].englishName}</h2>
-        </Link>
-        <p>{genre.name}</p>
-        <p>{releaseDate}</p>
-        {label &&
-          <Link to={`/label/${label.slug}`}>
-            <p>{label.englishName}</p>
-          </Link>
-        }
-        <img src={`${process.env.IMAGE_BASE_URL}${cover[0].url}`} alt=""/>
-      </Container>
+      <Banner>
+        <BannerWrap>
+          <TextWrap>
+            <Link to={`/artist/${artists[0].slug}`}>
+              <h2>{artists[0].englishName}</h2>
+            </Link>
+            <h1>{englishTitle}</h1>
+            {japaneseTitle && <h2>{japaneseTitle}</h2>}
+            <p>{genre.name}</p>
+            <p>{releaseDate}</p>
+            {label &&
+              <Link to={`/label/${label.slug}`}>
+                <p>{label.englishName}</p>
+              </Link>
+            }
+          </TextWrap>
+          <ImgWrap>
+            <img src={`${process.env.IMAGE_BASE_URL}${cover[0].url}`} alt=""/>
+          </ImgWrap>
+        </BannerWrap>
+      </Banner>
+      <Content>
+      </Content>
     </MainLayout>
   )
 };
